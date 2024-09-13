44674e0c52ef366bc2c09575678754fd3fe00ba7
James Robertson <jaarobertson92@gmail.com>
2024-09-13 14:08:03 +0100

    random color function

--- script.js
+++ script.js
@@ -4,9 +4,19 @@ const gridContainer = document.querySelector(".grid-container");
 // size of the grid
 const size = 16 * 16;
 
+// function for getting a random colour
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 // loop for adding the squares inside the grid container
 for (let i = 0; i < size; i++) {
-    const div = document.createElement("div");
-    div.classList.add("square");
-    gridContainer.appendChild(div);
+  const div = document.createElement("div");
+  div.classList.add("square");
+  gridContainer.appendChild(div);
 }
